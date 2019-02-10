3e19ea521e71b71ca8e1b9da378c62236e05e307
brendon-ng <brendonn8@gmail.com>
2019-02-10 04:54:14 -0800

    Text input

--- Assignment4/sfrob.c
+++ Assignment4/sfrob.c
@@ -2,17 +2,73 @@
 #include <stdlib.h>
 
 int frobcmp(char const * a, char const * b);
+void checkInputError(void);
+void checkOutputError(void);
 
 int main(void) {
-    char** allLines = (char **) malloc(sizeof(char));
     char* curLine = (char *) malloc(sizeof(char));
+    char** allLines = (char **) malloc(sizeof(char*));
     char cur = getchar();
+    checkInputError();
     
-    curLine[0] = cur;
+    int iLetter = 0;
+    int iLine = 0;
+    while(!feof(stdin)){
+        curLine[iLetter] = cur;
+        curLine = (char*) realloc(curLine, (iLetter+2) * sizeof(char));
+        if(curLine == NULL) {
+            free(curLine);
+            fprintf(stderr, "Error with memory allocation!");
+            exit(1);
+        }
+        iLetter++;
+        
+        // If we just added a space byte
+        if(cur == ' ') {
+            allLines[iLine] = curLine;
+            allLines = (char**) realloc(allLines, (iLine+2)*sizeof(char*));
+            if(allLines == NULL) {
+                free(allLines);
+                fprintf(stderr, "Error with memory allocation!");
+                exit(1);
+            }
+            iLine++;
+            free(curLine);
+            curLine = (char*) malloc(sizeof(char));
+            iLetter = 0;
+            
+            do {
+                cur = getchar();
+                checkInputError();
+                if(feof(stdin))
+                    break;
+            } while (cur == ' ');
+            continue;
+        }
+        
+        cur = getchar();
+        checkInputError();
+        if(feof(stdin)){
+            curLine[iLetter] = ' ';
+            allLines[iLine] = curLine;
+            allLines = (char**) realloc(allLines, (iLine+2)*sizeof(char*));
+            if(allLines == NULL) {
+                free(allLines);
+                fprintf(stderr, "Error with memory allocation!");
+                exit(1);
+            }
+            iLine++;
+            free(curLine);
+            curLine = (char*) malloc(sizeof(char));
+            iLetter = 0;
+        }
+        
+    }
     
     
     
-    printf("%c", curLine[0]);
+    
+    printf("%s", curLine);
     
     free(curLine);
     free(allLines);
@@ -31,6 +87,21 @@ int frobcmp(char const * a, char const * b) {
         return 1;
     else
         return -1;
+}
+
+
+void checkInputError() {
+    if (ferror(stdin) != 0) {
+        fprintf(stderr, "Error with reading input!");
+        exit(1);
+    }
+}
+
+void checkOutputError() {
+    if (ferror(stdin) != 0) {
+        fprintf(stderr, "Error with printing output!");
+        exit(1);
+    }
 }
 
 
