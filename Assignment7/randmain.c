a1a4f869bfb1889cb17e387b51a5a63a6b9d260d
brendon-ng <brendonn8@gmail.com>
2019-03-02 21:15:40 -0800

    finish makefile

--- Assignment7/randmain.c
+++ Assignment7/randmain.c
@@ -31,6 +31,22 @@
 #include <dlfcn.h>
 #include "randcpuid.h"
 
+static bool
+writebytes (unsigned long long x, int nbytes)
+{
+  int ndigits = nbytes * 2;
+  do
+    {
+      if (putchar ("0123456789abcdef"[x & 0xf]) < 0)
+	return false;
+      x >>= 4;
+      ndigits--;
+    }
+  while (0 < ndigits);
+
+  return 0 <= putchar ('\n');
+}
+
 
 /* Main program, which outputs N bytes of random data.  */
 int
@@ -40,20 +56,20 @@ main (int argc, char **argv)
   bool valid = false;
   long long nbytes;
   if (argc == 2)
-    {
-      char *endptr;
-      errno = 0;
-      nbytes = strtoll (argv[1], &endptr, 10);
-      if (errno)
-	perror (argv[1]);
-      else
-	valid = !*endptr && 0 <= nbytes;
-    }
+  {
+    char *endptr;
+    errno = 0;
+    nbytes = strtoll (argv[1], &endptr, 10);
+    if (errno)
+      perror (argv[1]);
+    else
+      valid = !*endptr && 0 <= nbytes;
+  }
   if (!valid)
-    {
-      fprintf (stderr, "%s: usage: %s NBYTES\n", argv[0], argv[0]);
-      return 1;
-    }
+  {
+    fprintf (stderr, "%s: usage: %s NBYTES\n", argv[0], argv[0]);
+    return 1;
+  }
 
   /* If there's no work to do, don't worry about which library to use.  */
   if (nbytes == 0)
@@ -63,72 +79,70 @@ main (int argc, char **argv)
      appropriate library.  */
   void* handle;
   unsigned long long (*rand64) (void);
-  char* err;
   if (rdrand_supported ())
-    {
-      handle = dlopen("randlibhw.so", RTLD_LAZY);
+  {
+    handle = dlopen("randlibhw.so", RTLD_LAZY);
       
-      if(!handle) { // Check for errors
-	fprintf(stderr, "Error calling dlopen() on randlibhw.so - exit status:%s\n", dlerror());
-	return 1;
-      }
-      
-      rand64 = dlsym(handle, "rand64");
-
-      if(dlerror() != NULL) { // Check for errors
-	fprintf(stderr, "Error calling dlsym() - exit status:%s\n", dlerror());
-	return 1;
-      }
+    if(!handle) { // Check for errors
+      fprintf(stderr, "Error calling dlopen() on randlibhw.so - exit status:%s\n", dlerror());
+      return 1;
     }
-  else
-    {
-      handle = dlopen("randlibsw.so", RTLD_LAZY);
-
-      if(!handle) { // Check for errors
-        fprintf(stderr, "Error calling dlopen() on randlibsw.so - exit status:%s\n", dlerror());
-        return 1;
-      }
       
-      rand64 = dlsym(handle, "rand64");
+    rand64 = dlsym(handle, "rand64");
 
-      if(dlerror() != NULL) { // Check for errors
-	fprintf(stderr, "Error calling dlsym() - exit status:%s\n", dlerror());
-        return 1;
-      }
+    if(dlerror() != NULL) { // Check for errors
+      fprintf(stderr, "Error calling dlsym() - exit status:%s\n", dlerror());
+      return 1;
     }
+  }
+else
+{
+  handle = dlopen("randlibsw.so", RTLD_LAZY);
+
+  if(!handle) { // Check for errors
+    fprintf(stderr, "Error calling dlopen() on randlibsw.so - exit status:%s\n", dlerror());
+    return 1;
+  }
+      
+  rand64 = dlsym(handle, "rand64");
+
+  if(dlerror() != NULL) { // Check for errors
+    fprintf(stderr, "Error calling dlsym() - exit status:%s\n", dlerror());
+    return 1;
+  }
+}
 
   int wordsize = sizeof rand64 ();
   int output_errno = 0;
 
   do
+  {
+    unsigned long long x = rand64 ();
+    int outbytes = nbytes < wordsize ? nbytes : wordsize;
+    if (!writebytes(x, outbytes))
     {
-      unsigned long long x = rand64 ();
-      int outbytes = nbytes < wordsize ? nbytes : wordsize;
-      if (!writebytes (x, outbytes))
-	{
-	  output_errno = errno;
-	  break;
-	}
-      nbytes -= outbytes;
+      output_errno = errno;
+      break;
     }
+    nbytes -= outbytes;
+  }
   while (0 < nbytes);
 
   if(dlclose(handle))
-    {
-      fprintf(stderr, "Error calling dlclose() - exit status: %s\n", dlerror());
-      return 1;
-    }
+  {
+    fprintf(stderr, "Error calling dlclose() - exit status: %s\n", dlerror());
+    return 1;
+  }
   
   if (fclose (stdout) != 0)
     output_errno = errno;
 
   if (output_errno)
-    {
-      errno = output_errno;
-      perror ("output");
-      finalize ();
-      return 1;
-    }
+  {
+    errno = output_errno;
+    perror ("output");
+    return 1;
+  }
 
   
   return 0;
